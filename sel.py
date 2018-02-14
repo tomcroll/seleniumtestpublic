3f7bba8b74b81a03ae4c5e5fdfd75c049ddd95f2
Automation Engineer <tom.croll@unomee.com>
2018-02-14 16:56:24 +0000

    First Selenium Test

---
+++ sel.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+
+driver = webdriver.Firefox()
+driver.get("http://www.google.com")
+driver.quit()
+
+driver = webdriver.Ie()
+driver.get("http://www.google.com")
+driver.quit()
+
+driver = webdriver.Chrome()
+driver.get("http://www.google.com")
+driver.quit()
